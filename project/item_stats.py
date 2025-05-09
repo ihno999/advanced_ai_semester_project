0ac7f61e80dac3f63bed1575cfdc2d2600c224c5
Ihno Van de Sande <ihno.vandesande@student.ucll.be>
2025-05-09 19:56:39 +0200

    definitive project folder aangeamaakt sabegame.json and dungeon ai moved to this folder, item_stats.py made with list of items and import added for this list in dungeon ai

---
+++ project/item_stats.py
@@ -0,0 +1,10 @@
+item_stat_boosts = {
+    "wooden sword": {"strength": +2},
+    "iron sword": {"strength": +5},
+    "torch": {"intelligence": +1},
+    "amulet of wisdom": {"intelligence": +5},
+    "boots of speed": {"endurance": +2},
+    "helmet of resilience": {"defense": +2},
+    "wooden stick": {"strength": +1},
+    # Add more items as needed
+}
